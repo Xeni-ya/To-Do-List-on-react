8efd56a768f3d86ff9f406fe03683a7f927b42ea
Xeni-ya <budyuginaxeniya@gmail.com>
2025-01-18 23:48:50 +0300

    feat: working with the props

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,14 +5,32 @@ import React from 'react';
 
 function Content() {
 
-  const items = ["Посмотреть урок", "Позаниматься йогой", "Написать список покупок"]
+  const items = [
+    {
+      id: 1,
+      name: "Посмотреть урок",
+      isCheked: false,
+      color: "red"
+    },
+    {
+      id: 2,
+      name: "Позаниматься йогой",
+      isCheked: true,
+      color: "orange"
+    },
+    {
+      id: 3, name: "Написать список покупок",
+      isCheked: false,
+      color: "pink"
+    }
+  ]
 
   return (
     <React.Fragment className="{styles.content}">
       <ul>
         {
           items.map((item) =>
-            <ListItem key={item.id} name={item.name} />
+            <ListItem key={item.id} name={item.name} color={item.color} />
           )
         }
       </ul>
--- src/components/Listitem.jsx
+++ src/components/Listitem.jsx
@@ -2,10 +2,13 @@ import PropTypes from 'prop-types';
 import styles from './listItem.module.css';
 
 function ListItem(props) {
+
+  const { name, color } = props;
+
   return (
     <div className={styles.item}>
       <input type="checkbox" className="checkbox"></input>
-      <label className="{styles.label}">{props.name}</label>
+      <label className={styles.label}>{name}</label>
       <button className={styles.button}>x</button>
     </div>
   );
