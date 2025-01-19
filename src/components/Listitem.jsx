6d1f686f8e798c14fd1dd16a9e0989bb724ae61a
Xeni-ya <budyuginaxeniya@gmail.com>
2025-01-19 22:47:04 +0300

    feat: working with the State

--- src/components/Listitem.jsx
+++ src/components/Listitem.jsx
@@ -1,13 +1,23 @@
 import PropTypes from 'prop-types';
 import styles from './listItem.module.css';
+import { useState } from 'react';
 
 function ListItem(props) {
+
+  const [cheked, setCheked] = useState(false)
+
+  const handleChekedState = () => {
+    setCheked(!cheked)
+  }
+
   return (
     <div className={styles.item} {...props}>
-      <input type="checkbox" className={styles.checkbox}></input>
-      <label className={{ color: props.color }}>{props.name}</label>
+      <input type="checkbox" className={styles.checkbox} onClick={handleChekedState}></input>
+      <label style={cheked
+        ? { textDecorationLine: 'line-through' }
+        : { textDecorationLine: 'none' }}>{props.name}</label>
       <button className={styles.button}>x</button>
-    </div>
+    </div >
   );
 }
 
