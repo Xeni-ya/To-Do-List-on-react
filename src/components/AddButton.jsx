0bf375d0ad6dd25aa24a81258299be1d140a18bc
Xeni-ya <budyuginaxeniya@gmail.com>
2025-02-25 20:40:08 +0300

    feat: adding a form to the 'addButton' component

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,21 +1,31 @@
 import styles from './addButton.module.css';
+import { useState } from 'react';
+import React from 'react';
 
 function AddButton() {
 
-  const handleClick = (e) => {
-    console.log("Hello", e.target)
-  }
+  // const handleClick = (e) => {
+  //   console.log("Hello", e.target)
+  // }
 
-  const handleClick1 = (name, e) => {
-    console.log("Hello" + name, e.target)
-  }
+  // const handleClick1 = (name, e) => {
+  //   console.log("Hello" + name, e.target)
+  // }
+
+  const [newItem, setNewItem] = useState("")
+  const [category, setCategory] = useState("")
 
   return (
-    <button className={styles.button} onClick={(e) => {
-      handleClick1("Maria", e)
-    }}>
-      Добавить
-    </button >
+    <form action="" className={styles.create}>
+      <input type="text" name="" id="" required value={newItem} onChange={(e) => setNewItem(e.target.value)} />
+      <select name="" id="" value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="studies">Учеба</option>
+        <option value="home">Домашние дела</option>
+      </select>
+      <button>Добавить</button>
+      <p>{category}</p>
+    </form>
+
   );
 }
 
--- src/components/Listitem.jsx
+++ src/components/Listitem.jsx
@@ -4,16 +4,16 @@ import { useState } from 'react';
 
 function ListItem(props) {
 
-  const [cheked, setCheked] = useState(false)
+  const [checked, setCheked] = useState(false)
 
-  const handleChekedState = () => {
-    setCheked(!cheked)
+  const handleСheckedState = () => {
+    setCheked(!checked)
   }
 
   return (
     <div className={styles.item} {...props}>
-      <input type="checkbox" className={styles.checkbox} onChange={handleChekedState}></input>
-      <label style={cheked
+      <input type="checkbox" className={styles.checkbox} checked={checked} onChange={handleСheckedState}></input>
+      <label style={checked
         ? { textDecorationLine: 'line-through' }
         : { textDecorationLine: 'none' }}>{props.name}</label>
       <button className={styles.button} onClick={() => props.deleteItem(props.id)}>x</button>
